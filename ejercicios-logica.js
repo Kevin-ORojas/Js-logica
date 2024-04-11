8f6087cea42a9b47cd2d3422cac22e8c366a85bd
NozoDev <kevinroj326@gmail.com>
2024-04-11 05:36:34 -0500

    secuencia de fibonacci

--- ejercicios-logica.js
+++ ejercicios-logica.js
@@ -94,3 +94,21 @@ function anagrama(palabra1, palabra2) {
 }
 
 console.log(anagrama("vaca", "cava"));
+
+/*
+ * Escribe un programa que imprima los 50 primeros números de la sucesión
+ * de Fibonacci empezando en 0.
+ * - La serie Fibonacci se compone por una sucesión de números en
+ *   la que el siguiente siempre es la suma de los dos anteriores.
+ *   0, 1, 1, 2, 3, 5, 8, 13...
+ */
+
+function fibonacci(n) {
+  let fib = [0, 1];
+  for (let i = 2; i <= 50; i++) {
+    fib.push(fib[i - 1] + fib[i - 2]);
+  }
+  return fib.slice(0, n);
+}
+const n = 20; // obetener los primeros 20 numeros de fibonacci
+console.log(fibonacci(n));
